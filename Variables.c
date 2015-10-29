721bdee1e5d29975f95976424b77ddae70f6e673
kturk <okulmavi@gmail.com>
2015-10-29 15:14:03 -0500

    Xcode Try

---
+++ Variables.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+
+	int age;
+	int currentYear;
+	int birthYear;
+
+	currentYear = 2015;
+	birthYear = 1997;
+	age = currentYear - birthYear;
+
+	printf("Kemal is %d years old\n", age);
+
+	return 0;
+}
+
